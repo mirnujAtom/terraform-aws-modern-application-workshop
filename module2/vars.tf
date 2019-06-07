5065bdb8a0771e7e565d2c99ce5889e1e969d060
Andrii Cherkasov <mirnuj.atom@gmail.com>
2019-06-07 17:39:48 +0300

    cleanup

--- module2/vars.tf
+++
@@ -1,6 +0,0 @@
-variable "profile" {default = "personal"}
-variable "region" {default = "us-east-1"}
-variable "app_name" {default = "mypizdiuchky"}
-variable "environment" {default = "dev"}
-variable "app_version" {default = "latest"}
-variable "index_template" {default = "files/index.html.tpl"}
