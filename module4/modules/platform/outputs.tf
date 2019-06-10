86fa9967c58f1f3d57b3277993abfa89cedcd8cd
Andrii Cherkasov <mirnuj.atom@gmail.com>
2019-06-10 22:58:57 +0300

    moved nlb to private subnet

--- module4/modules/platform/outputs.tf
+++ module4/modules/platform/outputs.tf
@@ -1,4 +1,3 @@
-output "public_subnets" { value = "${module.vpc.public_subnets}" }
 output "private_subnets" { value = "${module.vpc.private_subnets}" }
 output "vpc_id" { value = "${module.vpc.vpc_id}" }
 output "vpc_cidr" { value = "${var.vpc_cidr}" }
